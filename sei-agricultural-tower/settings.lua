9d14666675313c0ab13066512e1ffe13d165e6bc
swartzwelderbenjamin-design <swartzwelder.benjamin@gmail.com>
2025-12-18 03:12:21 -0600

    Delete sei-agricultural-tower directory

--- sei-agricultural-tower/settings.lua
+++
@@ -1,9 +0,0 @@
-data:extend({
-    {
-        type = "bool-setting",
-        name = "sei-agricultural-tower-allow-landfill",
-        setting_type = "startup",
-        default_value = false,
-        order = "a",
-    },
-})
