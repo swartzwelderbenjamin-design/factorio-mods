c31f349336a085a1ea53d26642b6f1569628651f
swartzwelderbenjamin-design <swartzwelder.benjamin@gmail.com>
2025-12-18 03:13:15 -0600

    Delete sei-biochamber directory

--- sei-biochamber/prototypes/categories/fuel-category.lua
+++
@@ -1,7 +0,0 @@
-data:extend({
-    {
-        type = "fuel-category",
-        name = "nutrients",
-        fuel_value_type = {"description.nutrients-energy-value"},
-    },
-})
--- sei-biochamber/settings-updates.lua
+++
@@ -1,2 +0,0 @@
-data.raw["string-setting"]["sei-library-spoilage"].default_value = data.raw["string-setting"]["sei-library-spoilage"].default_value .. "," .. "sei-biochamber-enable-spoilage"
-data.raw["string-setting"]["sei-library-spoilage"].allowed_values = {data.raw["string-setting"]["sei-library-spoilage"].default_value}
